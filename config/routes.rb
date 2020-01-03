2fa4d50997928ececf9a47e0e4c597708f94b357
NikolaCzv <nikola.raicic@hotmail.com>
2020-01-03 09:12:29 -0600

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :tag_notes
-  resources :tags
-  resources :notes
-  resources :users
+  resources :tag_notes, only: [:index, :create, :update, :edit]
+  resources :tags, only: [:index, :create, :update, :edit]
+  resources :notes, only: [:index, :show, :create, :destroy, :update, :edit]
+  resources :users, only: [:index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
